349672e6a59ce09ed660b5baf5e33d6af47e2a1f
Thrivikram Bhat <thrivikram.bhat.v@oracle.com>
2025-04-01 16:30:39 +0530

    extraction of data

--- util/dataextract/views.py
+++ util/dataextract/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .data_extraction import extract_data
 
-# Create your views here.
+def home_view(request):
+    return HttpResponse("Welcome to the Utilization App!")
+def extract_data_view(request):
+    # Call the data extraction function
+    dfs = extract_data()
+
+    # Debugging: Print the shape of the DataFrames
+    print("WTD DataFrame shape:", dfs['WTD'].shape)
+    print("MTD DataFrame shape:", dfs['MTD'].shape)
+
+    # Convert the extracted data (WTD and MTD DataFrames) to HTML tables
+    wtd_html = dfs['WTD'].to_html(classes='table table-striped', index=False)
+    mtd_html = dfs['MTD'].to_html(classes='table table-striped', index=False)
+
+    # Combine the HTML tables into a single response
+    html_content = f"""
+    <html>
+        <head>
+            <title>Extracted Data</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+            <div class="container">
+                <h1>Extracted WTD Data</h1>
+                {wtd_html}
+                <h1>Extracted MTD Data</h1>
+                {mtd_html}
+            </div>
+        </body>
+    </html>
+    """
+
+    return HttpResponse(html_content)
--- util/util/urls.py
+++ util/util/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from dataextract.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', home_view, name='home'),
+    path('dataextract/', include('dataextract.urls')),  # Include dataextract app URLs
 ]
