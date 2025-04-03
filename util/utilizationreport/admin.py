97be602d86c8243ff4066af264b09f8b01c2f9ea
Thrivikram Bhat <thrivikram.bhat.v@oracle.com>
2025-04-03 14:50:42 +0530

    admin rehgistration

--- util/utilizationreport/admin.py
+++ util/utilizationreport/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import UtilizationReport
+from .models import (
+    UtilizationReport,
+    Project,
+    Requirement,
+    Resource,
+    Allocation,
+    Deal,
+    ProjectActivityLog,
+)
 
+# Register your models here
 admin.site.register(UtilizationReport)
+admin.site.register(Project)
+admin.site.register(Requirement)
+admin.site.register(Resource)
+admin.site.register(Allocation)
+admin.site.register(Deal)
+admin.site.register(ProjectActivityLog)
